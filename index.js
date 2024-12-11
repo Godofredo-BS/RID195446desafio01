f0983913ee12bed582097e47020c57211eebd250
Godofredo=BS <godofredoborboremati@gmail.com>
2024-12-10 21:59:38 -0300

    Landingpagedesafio1

---
+++ index.js
@@ -0,0 +1,8 @@
+var rolamentoparadireita = window.document.getElementById("rolamentoparadireita")
+var rolamentoparaesquerda = window.document.getElementById("rolamentoparaesquerda")
+var titulomenuinferior = window.document.getElementById("titulomenu-inferior")
+function Rolarparadireita(){
+    titulomenuinferior.style ="display:none"
+    rolamentoparadireita.style= "display:none"
+rolamentoparaesquerda.style = "display:flex"
+}
